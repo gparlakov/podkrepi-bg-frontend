4ca7c211f9dc8b842624edc5e83c80797afbfd86
nradkova <neliradkova@abv.bg>
2022-04-14 15:41:51 +0300

    new campaign form imported

---
+++ src/components/campaigns/CreateCampaignPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useTranslation } from 'next-i18next'
+import { Container } from '@mui/material'
+
+import Layout from 'components/layout/Layout'
+
+// import CampaignForm from './CampaignForm'
+import CampaignCreateForm from './create-form/CampaignCreateForm' // => stepper form
+
+export default function CreateCampaignPage() {
+  const { t } = useTranslation()
+
+  return (
+    <Layout
+      title={t('nav.campaigns.index')}
+      metaDescription={t('campaigns:form-heading')}
+      githubUrl="https://github.com/podkrepi-bg/frontend/tree/master/src/components/campaigns/CreateCampaignPage.tsx"
+      figmaUrl="https://www.figma.com/file/MmvFKzUv6yE5U2wrOpWtwS/Podkrepi.bg?node-id=5055%3A21345">
+      <Container maxWidth="sm">
+        <CampaignCreateForm />
+      </Container>
+    </Layout>
+  )
+}
